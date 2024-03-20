5ab815dbcf24ddd5ff0ce36c011a8a6b6c82f886
VarTy <talankina.v@mail.ru>
2024-03-20 22:19:38 +0300

    translation

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './app/App.tsx'
 import './index.css'
 
+import './app/i18n.ts'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
--- src/widgets/SugToAuth/SugToAuth.tsx
+++ src/widgets/SugToAuth/SugToAuth.tsx
@@ -1,12 +1,15 @@
 import { Link } from 'react-router-dom';
 import s from './SugToAuth.module.scss';
 import { Button } from '@mantine/core';
+import { useTranslation } from 'react-i18next';
 
 const SugToAuth = () => {
+    const {t} = useTranslation();
+    
     return (
         <div className={s.nav}>
-            <p>Уже есть аккаунт?</p>
-            <Button component={Link} to="/" variant="subtle">Войти</Button>
+            <p>{t("registration.have-acc")}</p>
+            <Button component={Link} to="/" variant="subtle">{t("registration.login")}</Button>
         </div>
     )
 }
