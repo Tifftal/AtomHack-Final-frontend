4df7d5b88924d440790611ba2b570b6064cd8f5d
VarTy <talankina.v@mail.ru>
2024-03-20 16:34:58 +0300

    main page

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,10 +1,27 @@
+import Navbar from '../../widgets/Navbar/Navbar';
 import s from './MainPage.module.scss';
 
 const MainPage = () => {
-    return(
+    return (
         <div className={s.main}>
-            MAINPAGE
+            <Navbar />
+            <div className={s.first}>
+                <h1>Nova-Terra</h1>
+            </div>
+            <div className={s.second}>
+                <p>
+                    The year is 2150. Andromeda Galaxy, Nova Terra planet
+                    In the era of space travel and interstellar colonies, humanity has reached new horizons in space exploration. The planet Nova Terra, rich in unique minerals, has become a key facility for the construction of nuclear power plants that provide energy not only to colonies on its surface, but also to long-range space expeditions.
+                </p>
+                <ul>Nova Terra is divided into 5 colonies:
+                    <li>Aquarion</li>
+                    <li>Green Maze</li>
+                    <li>Terramorph</li>
+                    <li>Crystals</li>
+                    <li>Desert Whirlwind</li>
+                </ul>
+            </div>
         </div>
     )
 }
-export default MainPage
+export default MainPage;
