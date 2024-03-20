2e1026ab73e4e0156cdfa49df335852cfb6702d2
kabanets Vladimir <capvok@yandex.ru>
2024-03-20 13:41:09 +0300

    rodion's fail...

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import '@mantine/core/styles.css';
-import './App.css';
-import { MantineProvider } from '@mantine/core';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Auth from './pages/Auth/Auth';
+import "@mantine/core/styles.css";
+import "./App.css";
+import { MantineProvider } from "@mantine/core";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Auth } from "@pages/Auth";
 
 function App() {
   return (
     <MantineProvider>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Auth />} />
+          <Route path="/" element={<Auth />} />
         </Routes>
       </BrowserRouter>
     </MantineProvider>
-
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { fileURLToPath } from 'node:url';
+import react from "@vitejs/plugin-react";
+import { dirname, resolve } from 'node:path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+      "@assets": resolve(__dirname, "src/assets"),
+      "@pages": resolve(__dirname, "src/pages"),
+    },
+  },
   plugins: [react()],
-})
+});
