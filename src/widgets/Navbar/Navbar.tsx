eb082220dd69c25def6354671934d0b7fbab0045
VarTy <talankina.v@mail.ru>
2024-03-20 16:39:36 +0300

    navigation

--- src/widgets/Navbar/Navbar.tsx
+++ src/widgets/Navbar/Navbar.tsx
@@ -1,11 +1,14 @@
 import { Button } from '@mantine/core';
 import s from './Navbar.module.scss';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+    const navigate = useNavigate();
+
     return(
         <div className={s.navbar}>
             <h2>Talankina Varvara</h2>
-            <Button variant='outline' classNames={{label: s.label}}>Выйти</Button>
+            <Button variant='outline' classNames={{label: s.label}} onClick={()=>navigate('/')}>Выйти</Button>
         </div>
     )
 }
